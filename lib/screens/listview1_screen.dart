465ffff996eec0d1b02afe635846bdf3b338f73f
Reales <rclarence123@gmail.com>
2023-02-25 07:02:51 -0500

    Se agrega list tile al listView

--- lib/screens/listview1_screen.dart
+++ lib/screens/listview1_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ListView1Screen extends StatelessWidget {
+  final options = const ["Megaman", "Halo", "Fotnite", "Gear of wars"];
+
   const ListView1Screen({Key? key}) : super(key: key);
 
   @override
@@ -8,11 +10,13 @@ class ListView1Screen extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(title: const Text('ListView tipo 1')),
         body: ListView(
-          children: const [
-            Text('Hola mundo'),
-            Text('Hola mundo'),
-            Text('Hola mundo'),
-            Text('Hola mundo')
+          children: [
+            ...options
+                .map((game) => ListTile(
+                      title: Text(game),
+                      trailing: const Icon(Icons.arrow_forward_ios_outlined),
+                    ))
+                .toList()
           ],
         ));
   }
