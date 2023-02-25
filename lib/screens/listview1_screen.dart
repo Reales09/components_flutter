345ebbfa7f537aae51df008b491adf3bc64d1290
Reales <rclarence123@gmail.com>
2023-02-25 07:59:34 -0500

    Se crea list view con dividers y con estilos

--- lib/screens/listview1_screen.dart
+++ lib/screens/listview1_screen.dart
@@ -16,7 +16,8 @@ class ListView1Screen extends StatelessWidget {
                       title: Text(game),
                       trailing: const Icon(Icons.arrow_forward_ios_outlined),
                     ))
-                .toList()
+                .toList(),
+            Divider(),
           ],
         ));
   }
---
+++ lib/screens/listview2_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ListView2Screen extends StatelessWidget {
+  final options = const ["Megaman", "Halo", "Fotnite", "Gear of wars"];
+
+  const ListView2Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('ListView tipo 2'),
+          elevation: 0,
+          backgroundColor: Colors.indigo,
+        ),
+        body: ListView.separated(
+            itemBuilder: (context, index) => ListTile(
+                  title: Text(options[index]),
+                  trailing: const Icon(
+                    Icons.arrow_forward_ios_outlined,
+                    color: Colors.indigo,
+                  ),
+                  dense: false,
+                  onTap: () {
+                    final game = options[index];
+                    print(game);
+                  },
+                ),
+            separatorBuilder: (_, __) => const Divider(),
+            itemCount: options.length));
+  }
+}
