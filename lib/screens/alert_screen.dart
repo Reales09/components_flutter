00a443979fd6706d30f78d7fbb18d878bb9e941a
Reales <rclarence123@gmail.com>
2023-03-03 00:55:56 -0500

    Se agrega alert dialog

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -4,6 +4,35 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
 
+  void displayDialog(BuildContext context) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            elevation: 5,
+            title: const Text('Titulo'),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Text('Este es el contenido de la alerta'),
+                SizedBox(height: 10),
+                FlutterLogo(
+                  size: 100,
+                )
+              ],
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Cancelar'))
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,13 +49,11 @@ class AlertScreen extends StatelessWidget {
                 style: TextStyle(fontSize: 16),
               ),
             ),
-            onPressed: () {}),
+            onPressed: () => {displayDialog(context)}),
       ),
       floatingActionButton: FloatingActionButton(
           child: const Icon(Icons.close),
-          onPressed: () {
-            Navigator.pop(context);
-          }),
+          onPressed: () => Navigator.pop(context)),
     );
   }
 }
