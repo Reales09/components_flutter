79b23c125d232d6d8d6b7704450af7c9c5b98c23
Reales <rclarence123@gmail.com>
2023-03-04 10:07:15 -0500

    Centralizando inputTEXT

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class InputsScreen extends StatelessWidget {
@@ -7,41 +8,19 @@ class InputsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Inputs y forms'),
+        title: const Text('Inputs y forms'),
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: Column(children: [
-            TextFormField(
-              autofocus: false,
-              initialValue: '',
-              textCapitalization: TextCapitalization.words,
-              onChanged: (value) {
-                print('Value $value');
-              },
-              validator: (value) {
-                if (value == null) return 'Este campo es requerido';
-
-                return value.length < 3 ? 'Mínimo de 3 letras' : null;
-              },
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              decoration: const InputDecoration(
-                  hintText: 'nombre del usuario',
-                  labelText: 'Nombre',
-                  helperText: 'Sólo letras',
-                  // suffixIcon: Icon(Icons.group_outlined),
-                  prefixIcon: Icon(Icons.verified_user_outlined),
-                  icon: Icon(Icons.assignment_ind_outlined),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black)),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(10),
-                          topLeft: Radius.circular(10),
-                          topRight: Radius.circular(10)))),
-            )
-          ]),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Column(
+            children: const [
+              CustomInputField(
+                labelText: 'Nombre',
+                hintText: 'Escribe tu nombre',
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -5,29 +5,57 @@ class AppTheme {
   static const eerieBlack = Colors.black;
 
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-      //Color primario
-      primaryColor: eerieBlack,
+    //Color primario
+    primaryColor: eerieBlack,
 
-      //AppBarTheme
-      appBarTheme: const AppBarTheme(
-        color: eerieBlack,
-        elevation: 0,
+    //AppBarTheme
+    appBarTheme: const AppBarTheme(
+      color: eerieBlack,
+      elevation: 0,
+    ),
+    textButtonTheme:
+        TextButtonThemeData(style: TextButton.styleFrom(primary: primary)),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: primary,
+      elevation: 5,
+    ),
+
+    //elevatedButtons
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+          primary: Colors.black, shape: const StadiumBorder(), elevation: 0),
+    ),
+
+    // TextButton Theme
+
+    inputDecorationTheme: const InputDecorationTheme(
+      floatingLabelStyle: TextStyle(
+        color: primary,
       ),
-      textButtonTheme:
-          TextButtonThemeData(style: TextButton.styleFrom(primary: primary)),
-      floatingActionButtonTheme: const FloatingActionButtonThemeData(
-        backgroundColor: primary,
-        elevation: 5,
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: primary,
+        ),
+        borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(10), topRight: Radius.circular(10)),
       ),
-
-      //elevatedButtons
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-            primary: Colors.black, shape: const StadiumBorder(), elevation: 0),
-      )
-      // TextButton Theme
-
-      );
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: primary,
+        ),
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(10),
+          topRight: Radius.circular(10),
+        ),
+      ),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(10),
+          topRight: Radius.circular(10),
+        ),
+      ),
+    ),
+  );
 
   static final ThemeData darkTheme = ThemeData.dark().copyWith(
       //Color primario
